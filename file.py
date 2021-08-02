cdd6930018d742c33023dc333521a46277049281
Jeremy Okamoto <okamotojeremy@gmail.com>
2021-08-02 10:35:45 -0700

    a

---
+++ file.py
@@ -0,0 +1,9 @@
+"""
+name = ("jeremy")
+num = (1)
+print("hello " + name)
+print("hello ", + num )
+"""
+food1 = ("pizza")
+food2 = ("burger")
+print ("my favorite food is: " + food1 + " and " + food2)
